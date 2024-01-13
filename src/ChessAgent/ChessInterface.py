f5a5eb06ce01f6737a3147e8687e564dbad4e2db
Tomer Steinberg <tomerst77@gmail.com>
2024-01-13 22:22:51 +0200

    Add Chess interface functions

--- src/ChessAgent/ChessInterface.py
+++ src/ChessAgent/ChessInterface.py
@@ -1,9 +1,4 @@
-import sys
-import os
-import io
 import subprocess
-
-
 
 def get_options(process):
     """
@@ -18,7 +13,6 @@ def get_options(process):
     options = [process.stdout.readline()[-6:-2].decode()]
     while process.stdout.peek().decode() != 'DuckEngine> ':
         options.append(process.stdout.readline()[:4].decode())
-
     return options
 
 
@@ -27,34 +21,42 @@ def set_up_process():
     Sets up process
     @param: None
     @return: process
+    @rtype: Popen
     """
     process = subprocess.Popen("ChessCLIEngine.exe",
+                               bufsize=1024,
                                stdin=subprocess.PIPE,
                                stdout=subprocess.PIPE,
                                stderr=subprocess.PIPE,)
-
     process.stdout.readline()
     process.stdout.readline()
     process.stdin.write(b"create;\n")
-    process.stdin.flush()
+    #process.stdout.read(len(process.stdout.peek()))
     return process
 
 
 def move(process, move):
+    """
+    Interface for playing a move
+    @param: Process and move to play 
+    @return: status
+    @rtype: bool
+    """
     process.stdin.write(f"move {move};\n".encode())
     process.stdin.flush()
 
-    print("here: " + process.stdout.peek().decode())
+    valid = b':' not in process.stdout.read(len(process.stdout.peek()))
+    if process.stdout.peek() != b'DuckEngine> ':
+        valid = b':' not in process.stdout.read(len(process.stdout.peek()))
+    return valid
 
 
-def get_dump(process):
-    pass
+def get_dump(process, flag=''):
+    """"""
+    process.stdin.write(f"dump {flag};\n".encode())
+    process.stdin.flush()
 
-
-process = set_up_process()
-move(process, "a2a5")
-move(process, "e2e4")
-move(process, "a7a5")
-
-
-process.kill()
+    board_dump = [process.stdout.readline().decode().split("DuckEngine> ")[-1][:-2]]
+    while process.stdout.peek().decode() != 'DuckEngine> ':
+        board_dump.append(process.stdout.readline().decode()[:-2])
+    return board_dump
